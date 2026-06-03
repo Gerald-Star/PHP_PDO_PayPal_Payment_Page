97948e89040aaaf6df07ba3dddf438129910766b
Gerald-Star <ukwuomagerald@gmail.com>
2026-06-03 06:48:40 +0200

    creating Config file for database connection using PDO

---
+++ README.md
@@ -0,0 +1,9 @@
+## PDO error mode -search on Google on PHP documentation
+
+Youtube _ How to Create a PDO instance and set the error mode
+
+https://www.php.net/manual/en/pdo.error-handling.php
+
+
+Fetch Modes in PHP
+https://www.php.net/manual/en/pdo.constants.fetch-modes.php
---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+//host name
+define('HOST', 'localhost');
+
+//dbname 
+define('DBNAME', 'db_payment_page_paypal');
+
+//username
+define('USERNAME', 'root');
+
+//password
+define('PASSWORD', '');
+
+//create object to connect database using PDO
+try {
+    $pdo = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME, USERNAME, PASSWORD);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+
+
+
+
+
+
+
+?>
